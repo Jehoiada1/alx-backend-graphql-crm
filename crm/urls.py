e39652a0fd9e8a5361c92bac9bff0b6bafae57f6
Yodahe Eshetu <yodaheeshetu880@gmail.com>
2025-10-03 20:40:00 +0300

    Add trailing-slash variant for /graphql/ to avoid URL mismatch in browsers/checkers

--- crm/urls.py
+++ crm/urls.py
@@ -6,4 +6,6 @@ from django.views.decorators.csrf import csrf_exempt
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('graphql', csrf_exempt(GraphQLView.as_view(graphiql=True))),
+    # Support trailing slash variant as well
+    path('graphql/', csrf_exempt(GraphQLView.as_view(graphiql=True))),
 ]
