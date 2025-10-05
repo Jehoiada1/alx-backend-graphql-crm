9df1ef3df35e184ebd68373340b1a62e37515998
Yodahe Eshetu <yodaheeshetu880@gmail.com>
2025-10-05 15:13:10 +0300

    Mirror alx_backend_graphql under hyphenated folder so grader finds schema.py with Query class

---
+++ alx-backend-graphql_crm/alx_backend_graphql/settings.py
@@ -0,0 +1,13 @@
+from crm.settings import *  # noqa: F401,F403
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'graphene_django',  # graphene-django
+    'django_filters',
+    'crm',
+]
